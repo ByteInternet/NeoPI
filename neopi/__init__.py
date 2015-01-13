efc6ddd99a1e33214f0e8769806589211d974684
Rik van Achterberg <rik@byte.nl>
2015-01-13 15:23:57 +0100

    Packaging

---
+++ neopi/__init__.py
@@ -0,0 +1,1 @@
+from neopi import *
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(name="neopi",
+      version="0.1",
+      description="Byte-patched version of NeoPI",
+      url="https://github.com/ByteInternet/NeoPI",
+      packages=["neopi"],
+      author="Rik",
+      author_email="rik@byte.nl",
+      install_requires=[],
+      )
