85f626347db90b90bf512496f4154d068c5e118c
Rik <rvachterberg@gmail.com>
2015-01-27 11:28:03 +0100

    neopi -> neopitool in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-setup(name="neopi",
+setup(name="neopitool",
       version="0.1",
       description="Byte-patched version of NeoPI",
       url="https://github.com/ByteInternet/NeoPI",
